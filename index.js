ba1c9bc42ea3f517d7b7923354fc14e25c759fa9
Georgeches <georgechesire83@gmail.com>
2023-03-20 16:53:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+function multiply() {
+    let num1 = 31
+    return num1 * 2;
+}
+  
+function random() {
+    return Math.random() * 100 + 1;
+}
+  
+function mod() {
+    let num3 = 10
+    return num3 % 6
+}
+  
+function max() {
+    return Math.max.apply(null, [12, 11, 20]);
+}
 
-// Write your code here
+console.log(multiply())
+console.log(random())
+console.log(mod())
+console.log(max())
